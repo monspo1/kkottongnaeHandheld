81ed13a5feb1e7bae6c1911e1f8d09f12acf4605
SUNG PIL MOON <sungmoon@sungs-mbp.lan>
2020-01-20 15:34:13 -0500

    clean up code0

--- src/store/store.js
+++ src/store/store.js
@@ -6,8 +6,6 @@ import thunk from "redux-thunk";
 
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// const store = createStore(rootReducer)
-// const store = createStore(combineReducers(rootReducer));
 const store = createStore(
   rootReducer,
   storeEnhancers(applyMiddleware(mainMiddleware, thunk))
